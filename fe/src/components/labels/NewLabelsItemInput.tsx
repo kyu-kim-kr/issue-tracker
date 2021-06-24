35dd437f1e132eb7c1260ebaa5ab592b7bcea0c8
unknown <eamon3481@naver.com>
2021-06-24 17:03:51 +0900

    [#83] style: 새로운 라벨 추가 UI 컴포넌트 생성

---
+++ fe/src/components/labels/NewLabelsItemInput.tsx
@@ -0,0 +1,128 @@
+import Input from 'components/common/Input';
+import Label from 'components/common/Label';
+import CreateButton from 'components/buttons/CreateButton';
+import { ReactComponent as PlusSvg } from 'icons/plus.svg';
+import { ReactComponent as XSvg } from 'icons/Xicon.svg';
+import { ChangeEvent } from 'react';
+import styled from 'styled-components';
+import LabelTextColorInput from './LabelTextColorInput';
+import LabelColorInput from './LabelColorInput';
+import { LabelItemType } from 'types/issueType';
+import { useReducer } from 'react';
+import { labelUpdateAtom } from 'store';
+import { useSetRecoilState } from 'recoil';
+import axios from 'axios';
+import { labelReducer } from 'utils/reducer';
+import { labelParser } from 'utils/util';
+
+
+
+const NewLabelsItemInput = () => {
+  const [labelState, setLabelState] = useReducer(labelReducer, {
+    id : 0 ,
+    title : "새로운레이블",
+    description : "",
+    labelColor : "#185ADB",
+    textColor : "light",
+  });
+
+  const editClickHandler = (e: React.MouseEvent) => {}
+  const textColorClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const type = 'TextColor';
+    const payload = e.currentTarget.id === 'dark' ? 'dark' : 'light';
+    setLabelState({ type, payload });
+  };
+//   const setLabelUpdate = useSetRecoilState(labelUpdateAtom);
+
+//   const editClickHandler = (e: React.MouseEvent) => {
+//     (async function () {
+//       await axios.put(`${process.env.REACT_APP_API_URL}/api/labels/${id}`, {
+//         title: labelState.title,
+//         description: labelState.description,
+//         color_code: labelState.labelColor,
+//         font_light: labelState.textColor === 'light',
+//       });
+//       setLabelUpdate((cur) => ++cur);
+//     })();
+//   };
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const type = labelParser(e.target.getAttribute('aria-label'));
+    const payload = e.target.value;
+
+    if (type !== 'TextColor') {
+      setLabelState({ type, payload });
+    }
+  };
+
+  return (
+    <StyledLabelsItemInput> 
+      <LabelsInputDisplay>
+        <Label
+          title={labelState.title}
+          labelColor={labelState.labelColor}
+          textColor={labelState.textColor}
+        />
+      </LabelsInputDisplay>
+      <LabelsInputs>
+        <Input
+          label="레이블 이름"
+          onChange={changeHandler}
+          value={labelState.title}
+        />
+        <Input
+          label="설명(선택)"
+          onChange={changeHandler}
+          value={labelState.description}
+        />
+        <LabelColorSection>
+          <LabelColorInput
+            onChange={changeHandler}
+            value={labelState.labelColor}
+          />
+          <LabelTextColorInput
+            clickHandler={textColorClickHandler}
+            value={labelState.textColor}
+          />
+        </LabelColorSection>
+        <EditButtons>
+          <CreateButton onClick={editClickHandler} icon={<PlusIcon />}>
+            완료
+          </CreateButton>
+        </EditButtons>
+      </LabelsInputs>
+    </StyledLabelsItemInput>
+  );
+};
+
+export default NewLabelsItemInput;
+
+const LabelColorSection = styled.div`
+  display: flex;
+`;
+
+const StyledLabelsItemInput = styled.div`
+  display: flex;
+`;
+
+const LabelsInputs = styled.div`
+  ${({ theme }) => theme.style.flexColumn}
+  width: 75%;
+`;
+
+const LabelsInputDisplay = styled.div`
+  width: 25%;
+  ${({ theme }) => theme.style.flexCenter}
+`;
+
+const EditButtons = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 1rem;
+`;
+
+const PlusIcon = styled(PlusSvg)`
+  path {
+    stroke: inherit;
+  }
+`;
